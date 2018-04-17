4d6d248d93021799cdfcc032a0db9e870318ce2e
George Kaplan <george@georgekaplan.xyz>
2018-04-16 23:32:59 -0400

    tui example code for two bordered boxes in terminal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+extern crate tui;
+
+use std::io;
+
+use tui::Terminal;
+use tui::backend::RawBackend;
+use tui::widgets::{Widget, Block, Borders};
+use tui::layout::{Group, Size, Direction};
+
 fn main() {
-    println!("budgt");
+    let mut terminal = init().expect("Failed initialization.");
+    draw(&mut terminal).expect("Failed to draw");
+}
+
+fn init() -> Result<Terminal<RawBackend>, io::Error> {
+    let backend = RawBackend::new().unwrap();
+    Terminal::new(backend)
+}
+
+fn draw(t: &mut Terminal<RawBackend>) -> Result<(), io::Error> {
+    let size = t.size()?;
+
+    Group::default()
+        .direction(Direction::Vertical)
+        .margin(1)
+        .sizes(&[Size::Percent(10), Size::Percent(80), Size::Percent(10)])
+        .render(t, &size, |t, chunks| {
+            Block::default()
+                .title("Block")
+                .borders(Borders::ALL)
+                .render(t, &chunks[0]);
+            Block::default()
+                .title("Block 2")
+                .borders(Borders::ALL)
+                .render(t, &chunks[2]);
+        });
+
+    t.draw()
 }
