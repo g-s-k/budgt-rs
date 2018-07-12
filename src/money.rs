be33adf815b28e867a60f0bc32996bce32f4a3b4
George Kaplan <george@georgekaplan.xyz>
2018-07-11 22:05:59 -0400

    moved Money type into its own module
    all those impl blocks were getting in the way

---
+++ src/money.rs
@@ -0,0 +1,93 @@
+use std::{fmt, ops};
+
+#[derive(Clone, Copy)]
+pub struct Money(pub i64);
+
+impl ops::Deref for Money {
+    type Target = i64;
+
+    fn deref(&self) -> &i64 {
+        &self.0
+    }
+}
+
+impl ops::DerefMut for Money {
+    fn deref_mut(&mut self) -> &mut i64 {
+        &mut self.0
+    }
+}
+
+impl<T> From<T> for Money
+where
+    i64: From<T>,
+{
+    fn from(val: T) -> Self {
+        Money(i64::from(val))
+    }
+}
+
+impl<T> ops::Add<T> for Money
+where
+    Money: From<T>,
+{
+    type Output = Money;
+
+    fn add(self, rhs: T) -> Money {
+        let r = Money::from(rhs);
+        Money(self.0 + r.0)
+    }
+}
+
+impl<T> ops::Sub<T> for Money
+where
+    Money: From<T>,
+{
+    type Output = Money;
+
+    fn sub(self, rhs: T) -> Money {
+        let r = Money::from(rhs);
+        Money(self.0 - r.0)
+    }
+}
+
+impl<T> ops::Mul<T> for Money
+where
+    Money: From<T>,
+{
+    type Output = Money;
+
+    fn mul(self, rhs: T) -> Money {
+        let r = Money::from(rhs);
+        Money(self.0 * r.0)
+    }
+}
+
+impl<T> ops::Div<T> for Money
+where
+    Money: From<T>,
+{
+    type Output = Money;
+
+    fn div(self, rhs: T) -> Money {
+        let r = Money::from(rhs);
+        Money(self.0 / r.0)
+    }
+}
+
+impl<T> ops::Rem<T> for Money
+where
+    Money: From<T>,
+{
+    type Output = Money;
+
+    fn rem(self, rhs: T) -> Money {
+        let r = Money::from(rhs);
+        Money(self.0 % r.0)
+    }
+}
+
+impl fmt::Display for Money {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:5}.{:02}", *self / 100, *self % 100)
+    }
+}
